b8a0e5c50bb77cfa476925eba2f51572db9cbc14
Claas <claas.moehlmann@outlook.com>
2024-01-16 01:01:57 +0100

    Proof of concept nested scroll

--- main.js
+++ main.js
@@ -1,48 +1,25 @@
-const lowerRow = document.getElementById("lower-row");
-const middleRow = document.getElementById("middle-row");
-lowerRow.addEventListener("scroll", () => {
-  // Percentage
-  const percentage =
-    lowerRow.scrollLeft / (lowerRow.scrollWidth - lowerRow.clientWidth);
-  console.debug(percentage, lowerRow.scrollLeft, lowerRow.scrollWidth);
+// const lowerRow = document.getElementById("lower-row");
+// const middleRow = document.getElementById("middle-row");
+// lowerRow.addEventListener("scroll", () => {
+//   // Percentage
+//   const percentage =
+//     lowerRow.scrollLeft / (lowerRow.scrollWidth - lowerRow.clientWidth);
+//   console.debug(percentage, lowerRow.scrollLeft, lowerRow.scrollWidth);
 
-  //   middleRow.scrollTo({
-  //     left: lowerRow.scrollLeft,
-  //     behavior: "instant",
-  //   });
-  scrollToPercentage(percentage);
-});
-
-/**
- *
- * @param {number} percentage
- */
-function scrollToPercentage(percentage) {
-  middleRow.scrollTo({
-    left: (middleRow.scrollWidth - middleRow.clientWidth) * percentage,
-    behavior: "instant",
-  });
-}
+//   //   middleRow.scrollTo({
+//   //     left: lowerRow.scrollLeft,
+//   //     behavior: "instant",
+//   //   });
+//   scrollToPercentage(percentage);
+// });
 
 // /**
-//  * @type {IntersectionObserverInit}
+//  *
+//  * @param {number} percentage
 //  */
-// const options = {
-//   root: document.getElementById("lower-row"),
-//   rootMargin: "0px",
-//   threshold: 0.1,
-// };
-
-// const observer = new IntersectionObserver((entries) => {
-//   //   entries.forEach((entry) => {
-//   //     console.debug(entry);
-//   //   });
-
-//   console.debug(entries[0].intersectionRatio);
-// }, options);
-
-// const boxes = document.querySelectorAll("#lower-row .box");
-
-// boxes.forEach((box) => {
-//   observer.observe(box);
-// });
+// function scrollToPercentage(percentage) {
+//   middleRow.scrollTo({
+//     left: (middleRow.scrollWidth - middleRow.clientWidth) * percentage,
+//     behavior: "instant",
+//   });
+// }
